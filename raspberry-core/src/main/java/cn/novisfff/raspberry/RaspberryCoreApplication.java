6885a565cb7f134e278dcf9f5f5e0e7f8708b6a5
novisfff <156125813@qq.com>
2020-12-12 15:13:44 +0800

    创建core模块

---
+++ raspberry-core/src/main/java/cn/novisfff/raspberry/RaspberryCoreApplication.java
@@ -0,0 +1,16 @@
+package cn.novisfff.raspberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：<a href="156125813@qq.com">novisfff</a>
+ */
+@SpringBootApplication
+public class RaspberryCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberryCoreApplication.class, args);
+	}
+
+}
---
+++ raspberry-core/src/test/java/cn/novisfff/raspberry/RaspberryCoreApplicationTests.java
@@ -0,0 +1,15 @@
+package cn.novisfff.raspberry;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Ignore
+public class RaspberryCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
