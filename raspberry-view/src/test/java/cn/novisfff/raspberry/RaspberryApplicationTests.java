cd13d218a10f395f700c79b3cb200cd68384fda9
novisfff <156125813@qq.com>
2020-12-24 15:09:56 +0800

    将项目从maven迁移到gradle

--- raspberry-view/src/test/java/cn/novisfff/raspberry/RaspberryApplicationTests.java
+++ raspberry-view/src/test/java/cn/novisfff/raspberry/RaspberryApplicationTests.java
@@ -1,11 +1,9 @@
 package cn.novisfff.raspberry;
 
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Ignore
 public class RaspberryApplicationTests {
 
     @Test
